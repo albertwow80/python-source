434ec9a1d01358641a6ea4045800900f316d1eab
Tejas Manohar <me@tejas.io>
2016-06-14 12:08:55 -0500

    move from fb ads

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+import os
+
+setup(
+    name = 'source',
+    version = '0.0.0',
+    packages=[
+        'source'
+    ],
+    install_requires=[
+        'jsonrpc-requests==0.2',
+        'simplejson==3.8.2',
+        'websocket-client==0.37.0'
+    ]
+)
